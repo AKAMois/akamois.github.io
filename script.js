634e2bb3b6da88488bdd0688789f3a92f1a0589c
Diego <diegomf099@gmail.com>
2024-02-28 13:59:27 +0100

    updated

---
+++ script.js
@@ -0,0 +1,6 @@
+const wrapper = document.querySelector('.wrapper');
+const register = document.querySelector('.register-link');
+
+
+register.addEventListener('click', () => wrapper.classList.add('active'));
+
